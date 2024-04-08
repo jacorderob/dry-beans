f52551d29fcf265f1c37f355a2d8f94bac5d7893
Andrés Cordero <--get-all>
2024-04-08 19:12:02 -0400

    Feature: create trip task controller

--- app/controllers/trip_tasks_controller.rb
+++ app/controllers/trip_tasks_controller.rb
@@ -1,53 +1,19 @@
 class TripTasksController < ApplicationController
-  before_action :set_trip_task, only: %i[ show update destroy ]
-
-  # GET /trip_tasks
-  # GET /trip_tasks.json
-  def index
-    @trip_tasks = TripTask.all
-  end
-
-  # GET /trip_tasks/1
-  # GET /trip_tasks/1.json
-  def show
-  end
-
-  # POST /trip_tasks
   # POST /trip_tasks.json
   def create
     @trip_task = TripTask.new(trip_task_params)
 
     if @trip_task.save
-      render :show, status: :created, location: @trip_task
+      route = @trip_task.trip.delivery_route
+      render route, status: :created, location: route
     else
       render json: @trip_task.errors, status: :unprocessable_entity
     end
-  end
-
-  # PATCH/PUT /trip_tasks/1
-  # PATCH/PUT /trip_tasks/1.json
-  def update
-    if @trip_task.update(trip_task_params)
-      render :show, status: :ok, location: @trip_task
-    else
-      render json: @trip_task.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /trip_tasks/1
-  # DELETE /trip_tasks/1.json
-  def destroy
-    @trip_task.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_trip_task
-      @trip_task = TripTask.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def trip_task_params
-      params.require(:trip_task).permit(:trip_id, :kind, :adress, :adressee_name, :completed, :message)
+      params.require(:trip_task).permit(:trip_id, :kind, :address, :addressee_name, :completed, :message)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   resources :delivery_routes, only: :show
+  resources :trip_tasks, only: :create
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
